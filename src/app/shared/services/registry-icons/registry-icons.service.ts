93ee8c514f49fc050a4e3a259d451d6a664345e8
Thiago Martins de Freitas <martins86@gmail.com>
2021-02-13 21:29:58 -0300

    criando servico de registro icones svg

---
+++ src/app/shared/services/registry-icons/registry-icons.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RegistryIconsService {
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {}
+
+  public registryIcons(iconsNameRegistry: string[]): string[] {
+    iconsNameRegistry.forEach((iconName) => {
+      this.matIconRegistry.addSvgIcon(
+        iconName,
+        this.domSanitizer.bypassSecurityTrustResourceUrl(
+          './../../../assets/icons/svg/' + iconName + '.svg'
+        )
+      );
+    });
+    return iconsNameRegistry;
+  }
+}
