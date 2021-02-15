d03636cc77afef63e85c120fddf692f6d743f3f5
Thiago Martins de Freitas <martins86@gmail.com>
2021-02-15 16:59:13 -0300

    adicionando funcoes abre/fecha barra left

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppModule } from './app.module';
@@ -6,6 +6,9 @@ import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule, AppModule],
@@ -13,9 +16,60 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create the app component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe('Testing leftBarToggle', () => {
+    it('should the leftBarOpen value be equal to false', () => {
+      // Arrange
+      component.leftBarOpen = true;
+
+      // Act
+      component.leftBarToggle();
+
+      // Assert
+      expect(component.leftBarOpen).toBe(false);
+    });
+
+    it('should the leftBarOpen value be equal to true', () => {
+      // Arrange
+      component.leftBarOpen = false;
+
+      // Act
+      component.leftBarToggle();
+
+      // Assert
+      expect(component.leftBarOpen).toBe(true);
+    });
+  });
+
+  describe('Testing closeLeftBar', () => {
+    it('should the leftBarOpen value be equal to false', () => {
+      // Arrange
+      component.leftBarOpen = true;
+
+      // Act
+      component.closeLeftBar();
+
+      // Assert
+      expect(component.leftBarOpen).toBe(false);
+    });
+
+    it('should the value of leftBarOpen remain equal to false', () => {
+      // Arrange
+      component.leftBarOpen = false;
+
+      // Act
+      component.closeLeftBar();
+
+      // Assert
+      expect(component.leftBarOpen).toBe(false);
+    });
   });
 });
