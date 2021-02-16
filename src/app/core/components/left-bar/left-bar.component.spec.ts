405513897a7fdaddb51f99077d8d1608bcd18bfd
Thiago Martins de Freitas <martins86@gmail.com>
2021-02-16 20:07:57 -0300

    implementando menus e card de perfil

--- src/app/core/components/left-bar/left-bar.component.spec.ts
+++ src/app/core/components/left-bar/left-bar.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { LeftBarModule } from './left-bar.module';
 
@@ -10,7 +11,7 @@ describe('LeftBarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LeftBarModule],
+      imports: [RouterTestingModule, LeftBarModule],
       declarations: [LeftBarComponent],
     }).compileComponents();
   });
--- src/app/core/components/left-bar/left-bar.component.ts
+++ src/app/core/components/left-bar/left-bar.component.ts
@@ -1,12 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { RegistryIconsService } from 'src/app/shared/services/registry-icons/registry-icons.service';
 
 @Component({
   selector: 'app-left-bar',
   templateUrl: './left-bar.component.html',
   styleUrls: ['./left-bar.component.scss'],
 })
-export class LeftBarComponent implements OnInit {
-  constructor() {}
+export class LeftBarComponent {
+  constructor(protected iconRegistryService: RegistryIconsService) {
+    this.registeringIcons();
+  }
 
-  ngOnInit(): void {}
+  private registeringIcons(): void {
+    const iconsNames: string[] = [
+      'account_circle',
+      'home',
+      'login',
+      'manage_accounts',
+      'logout',
+    ];
+    this.iconRegistryService.registryIcons(iconsNames);
+  }
 }
