fbaa5a83d5cff09e7b338b54c1a3da9ac22a02c0
Thiago Martins de Freitas <martins86@gmail.com>
2021-03-03 23:11:15 -0300

    adicionando prop a model

--- src/app/core/auth/model/user.model.ts
+++ src/app/core/auth/model/user.model.ts
@@ -1,7 +1,20 @@
-export interface UserModel {
-  uid: string;
-  email: string;
-  photoURL?: string;
-  displayName?: string;
-  somethingCustom?: string;
+export class Roles {
+  constructor(
+    public subscriber?: boolean,
+    public editor?: boolean,
+    public admin?: boolean,
+
+  ) {}
+}
+
+export class UserModel {
+  constructor(
+    public uid: string,
+    public displayName: string,
+    public email: string,
+    public password: string,
+    public role: Roles,
+    public active: boolean,
+    public photoURL?: string
+  ) {}
 }
