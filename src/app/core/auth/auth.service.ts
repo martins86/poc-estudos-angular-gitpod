395fca890e3722e9f006ac1c4f77ca63de83be80
Thiago Martins de Freitas <martins86@gmail.com>
2021-03-03 23:50:09 -0300

    adicionando auth service

---
+++ src/app/core/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+import firebase from 'firebase/app';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(
+    private afAuth: AngularFireAuth,
+    private afDb: AngularFirestore
+  ) {}
+}
