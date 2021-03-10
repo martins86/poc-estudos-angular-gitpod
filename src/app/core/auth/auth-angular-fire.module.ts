601e952687a298c59518cf573477e8076650fc7c
Thiago Martins de Freitas <martins86@gmail.com>
2021-03-09 21:22:47 -0300

    testado update fire e var global

--- src/app/core/auth/auth-angular-fire.module.ts
+++ src/app/core/auth/auth-angular-fire.module.ts
@@ -3,20 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { environment } from './../../../environments/environment';
 
-// import { AngularFireModule } from '@angular/fire';
-// import { AngularFireAuthModule } from '@angular/fire/auth';
-// import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
-// import { GoogleSignInDirective } from './directive/google-sign-in.directive';
+import { GoogleSignInDirective } from './directive/google-sign-in.directive';
 
 @NgModule({
-  // declarations: [GoogleSignInDirective],
+  declarations: [GoogleSignInDirective],
   imports: [
     CommonModule,
-    // AngularFireModule.initializeApp(environment.firebase),
-    // AngularFireAuthModule,
-    // AngularFirestoreModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
   ],
-  // exports: [GoogleSignInDirective],
+  exports: [GoogleSignInDirective],
 })
 export class AuthAngularFireModule {}
